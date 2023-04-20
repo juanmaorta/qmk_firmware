bf691b54f8a6be53c19bcebf24a6c2de9ed55a5f
Juanma Orta <jorta@auvik.com>
2023-04-20 11:41:26 +0200

    crkbd ergokeebs: enable via

--- keyboards/crkbd/keymaps/ergokeebs/rules.mk
+++ keyboards/crkbd/keymaps/ergokeebs/rules.mk
@@ -1,7 +1,9 @@
-MOUSEKEY_ENABLE = yes    # Mouse keys
-RGBLIGHT_ENABLE = yes    # Enable WS2812 RGB underlight.
-OLED_ENABLE     = yes
+MOUSEKEY_ENABLE = no    # Mouse keys
+RGBLIGHT_ENABLE = no    # Enable WS2812 RGB underlight.
+OLED_ENABLE     = no
 OLED_DRIVER     = SSD1306
 LTO_ENABLE      = yes
 SPLIT_KEYBOARD  = yes
 CAPS_WORD_ENABLE = yes
+
+VIA_ENABLE        = yes
